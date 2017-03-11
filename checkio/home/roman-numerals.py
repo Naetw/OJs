f8b7907c2ce0ab4633adaff3d46c8e4b4838493f
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-03-11 12:08:42 +0800

    roman-numerals solved

---
+++ checkio/home/roman-numerals.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+
+ROMAN = (
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I'),
+        )
+
+def checkio(data):
+    roman_num = ''
+    for num, symbol in ROMAN:
+        while data >= num:
+            data -= num
+            roman_num += symbol
+    #replace this for solution
+    return roman_num
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
