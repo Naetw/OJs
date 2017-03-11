e812097f12a5e7d3ff7a7c4a36e111160f20870d
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-03-11 11:29:06 +0800

    non-unique-elements solved

---
+++ checkio/home/non-unique-elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+
+
+def checkio(data):
+    return [ element for element in data if data.count(element) > 1 ]
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
