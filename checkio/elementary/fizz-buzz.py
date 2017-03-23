f3b361549fb4cca2562d4753c873957c1436f02b
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-03-23 20:54:47 +0800

    Fizz Buzz solved.

---
+++ checkio/elementary/fizz-buzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+#Your optional code here
+#You can import some modules or create additional functions
+
+
+def checkio(number):
+    div = {3 : 'Fizz', 5 : 'Buzz'}
+    ans = ' '.join(div[i] if number%i == 0 else '' for i in div).strip()
+    return str(number) if ans == '' else ans
+
+#Some hints:
+#Convert a number in the string with str(n)
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert checkio(6) == "Fizz", "6 is divisible by 3"
+    assert checkio(5) == "Buzz", "5 is divisible by 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
