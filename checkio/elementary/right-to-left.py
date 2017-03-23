81097c9a9d490abdebd6d135dbc0fdfc837430f4
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-03-23 22:34:33 +0800

    Right to left solved.

---
+++ checkio/elementary/right-to-left.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+def left_join(phrases):
+    sub = lambda x : x.replace('right', 'left')
+    return ','.join(word for word in map(sub, phrases))
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop", "All to left"
+    assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
+    assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
+    assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
